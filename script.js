b45bb22296feaa3d8934f74f3a8f6a5a79ee7080
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-05-25 19:12:17 -0300

    atualizando o javascript

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ menuBtn.addEventListener("click", function(){
         linksContainer.style.height = 0
     }
 });
+
+//================fixando a navbar==========================
+const navbar = document.querySelector("#nav")
+const btnToUp = document.querySelector('.top-link')
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset
+    const navHeight = navbar.getBoundingClientRect().height
+    
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav')
+    }else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight > 500){
+        btnToUp.classList.add("show-link")
+    }else{
+        btnToUp.classList.remove("show-link")
+    }
+});
